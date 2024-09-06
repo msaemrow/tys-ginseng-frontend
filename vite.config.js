f151a241357c02f1a43810ea1ee52ff562d89c2a
Matt Saemrow <mattsaemrow@gmail.com>
2024-09-05 19:45:03 -0500

    updated base route

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
-    base: "/",
+    base: "/tys-ginseng-frontend",
   };
 });
