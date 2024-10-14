bdbc4a8bc0fb6e0124a008de4c7a82b5bf9a06e0
Matt Saemrow <mattsaemrow@gmail.com>
2024-10-14 08:40:12 -0500

    updated product page

--- src/components/Admin/ProductPage.jsx
+++ src/components/Admin/ProductPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router";
 import GinsengApi from "../../squareAPI/api";
+import "../../css/Admin/ProductPage.css";
 
 const ProductPage = () => {
   const { barcode } = useParams();
@@ -12,6 +13,7 @@ const ProductPage = () => {
     navigate("/admin/homepage");
   };
 
+  //fet product data
   useEffect(() => {
     const fetchProduct = async () => {
       try {
@@ -25,6 +27,7 @@ const ProductPage = () => {
     fetchProduct();
   }, [barcode]);
 
+  // Show spinner while waiting for product data
   if (!product) {
     return (
       <div
@@ -35,60 +38,78 @@ const ProductPage = () => {
           <span className="visually-hidden">Loading...</span>
         </div>
       </div>
-    ); // Show spinner while waiting for product data
+    );
   }
 
   return (
-    <div className="container pt-5 d-flex flex-column align-items-start">
-      <h1>{product.name} Details</h1>
-      <button onClick={clickReturnToProducts}>Return to all products</button>
-      <p>
-        <span className="fw-bold">Barcode: </span> {product.barcode}
-      </p>
-      <p>
-        <span className="fw-bold">Type: </span>
-        {product.type}
-      </p>
-      <p>
-        <span className="fw-bold">Name: </span>
-        {product.name}
-      </p>
-      <p>
-        <span className="fw-bold">Price: </span>
-        {product.price}
-      </p>
-      <p>
-        <span className="fw-bold">Sale Price: </span>
-        {product.sale_price}
-      </p>
-      <p>
-        <span className="fw-bold">On Sale? </span>
-        {product.on_sale ? "True" : "False"}
-      </p>
-      <p>
-        <span className="fw-bold">Description: </span>
-        {product.description}
-      </p>
-      <p>
-        <span className="fw-bold">Servings: </span>
-        {product.servings}
-      </p>
-      <p>
-        <span className="fw-bold">Image URL: </span>
-        {product.image_url}
-      </p>
-      <p>
-        <span className="fw-bold">Product Weight: </span>
-        {product.weight}
-      </p>
-      <p>
-        <span className="fw-bold">Quantity: </span>
-        {product.quantity}
-      </p>
-      <p>
-        <span className="fw-bold">Best Seller? </span>
-        {product.best_seller === true ? "True" : "False"}
-      </p>
+    <div className="container pt-5 ">
+      {/* div containing header and button to return to all products */}
+      <div className="d-flex justify-content-space-between align-items-center">
+        <h1 className="me-3">{product.name} Details</h1>
+        <button className="ms-3 h-75" onClick={clickReturnToProducts}>
+          Return to all products
+        </button>
+      </div>
+      <div className="d-flex justify-content-center align-items-center">
+        {/* div containing all product detail- left side of screen */}
+        <div className="d-flex flex-column align-items-start ps-5">
+          <p>
+            <span className="fw-bold">Barcode: </span> {product.barcode}
+          </p>
+          <p>
+            <span className="fw-bold">Type: </span>
+            {product.type}
+          </p>
+          <p>
+            <span className="fw-bold">Name: </span>
+            {product.name}
+          </p>
+          <p>
+            <span className="fw-bold">Price: </span>
+            {product.price}
+          </p>
+          <p>
+            <span className="fw-bold">Sale Price: </span>
+            {product.sale_price}
+          </p>
+          <p>
+            <span className="fw-bold">On Sale? </span>
+            {product.on_sale ? "True" : "False"}
+          </p>
+          <p>
+            <span className="fw-bold">Description: </span>
+            {product.description}
+          </p>
+          <p>
+            <span className="fw-bold">Servings: </span>
+            {product.servings}
+          </p>
+          <p>
+            <span className="fw-bold">Image URL: </span>
+            {product.image_url}
+          </p>
+          <p>
+            <span className="fw-bold">Product Weight: </span>
+            {product.weight}
+          </p>
+          <p>
+            <span className="fw-bold">Quantity: </span>
+            {product.quantity}
+          </p>
+          <p>
+            <span className="fw-bold">Best Seller? </span>
+            {product.best_seller === true ? "True" : "False"}
+          </p>
+        </div>
+        {/* div containing all product image- right side of screen */}
+        <div>
+          <img
+            className="ms-5 product-image rounded"
+            src={product.image_url}
+            alt={product.description}
+          />
+        </div>
+      </div>
     </div>
   );
 };
